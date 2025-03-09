82626d582b7c297902aacf26b8117d0da617992e
abdoulaye-sys <abdoulayeba98@outlook.com>
2025-03-09 18:00:43 +0100

    ssl

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg');
 
-// Vérifie si toutes les variables d'environnement nécessaires sont définies
 if (!process.env.DB_USER || !process.env.DB_HOST || !process.env.DB_NAME || !process.env.DB_PASS || !process.env.DB_PORT) {
     console.error('❌ Une ou plusieurs variables d\'environnement sont manquantes !');
-    process.exit(1);  // Arrête l'application si les variables ne sont pas définies
+    process.exit(1);
 }
 
 const pool = new Pool({
@@ -11,7 +10,10 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT || 5432,  // Utilise 5432 par défaut si DB_PORT n'est pas défini
+    port: process.env.DB_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: false,  // Accepte les connexions SSL sans vérifier le certificat
+    }
 });
 
 module.exports = pool;
