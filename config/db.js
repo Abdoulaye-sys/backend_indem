fbc9b4f90225da6ebf4dba59ee6267f57fdc9484
abdoulaye-sys <abdoulayeba98@outlook.com>
2025-03-09 17:34:53 +0100

    Fix deployment issue

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
+// Vérifie si toutes les variables d'environnement nécessaires sont définies
+if (!process.env.DB_USER || !process.env.DB_HOST || !process.env.DB_NAME || !process.env.DB_PASS || !process.env.DB_PORT) {
+    console.error('❌ Une ou plusieurs variables d\'environnement sont manquantes !');
+    process.exit(1);  // Arrête l'application si les variables ne sont pas définies
+}
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,  // Utilise 5432 par défaut si DB_PORT n'est pas défini
 });
 
 module.exports = pool;
